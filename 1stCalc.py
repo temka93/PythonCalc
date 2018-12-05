bec5d23eb7d0a19b2ee6bdda37eebfd6d55bb2b0
Administrator <Administrator@EC2AMAZ-LE68PGQ>
2018-12-05 19:01:35 +0000

    Initial commit

---
+++ 1stCalc.py
@@ -0,0 +1,21 @@
+
+
+def func_x():
+    print "Hello World"
+
+
+def func_y(x):
+    return 1
+
+
+
+def test_func():
+    assert func_y(3) == 1
+
+
+if __name__ == "__main__":
+    func_x()
+    func_y(666)
+    test_func()
+else:
+    func_y(6)
